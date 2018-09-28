b1ca6b06414df95b108f259efbe4f48c3305999c
skiddles <marks.technical@outlook.com>
2018-09-28 07:32:12 -0500

    Add support for modules

---
+++ __init__.py
@@ -0,0 +1,21 @@
+
+def descriptor(no_params):
+    """
+    Returns:
+        This returns no values.  It is here primarily to support modules, but also
+        contains the project TO DO list.
+
+    :param no_params:  This file takes no parameters
+
+        TODO: need to incorporate the following functionality.
+
+        1. Chart confidence bands in reporting and charts
+        2. Abstract the model definition
+        3. Save and Load the model
+        4. Enable training on the entire data set
+        5. Prediction only from new data
+        6. Enable grid searching for optimal parameters
+        7. Calculate Value at Risk by adding quantity held parameter.
+        8. Explore some convolutional aspects
+    """
+    pass
